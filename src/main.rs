279c5350277483c05f94a9890d2646372a54523f
Your Name <you@example.com>
2020-10-18 21:53:45 +0800

    Quick check

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
 
 use core::panic::PanicInfo;
 
 mod vga_buffer;
 
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
-}
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
@@ -16,3 +14,20 @@ pub extern "C" fn _start() -> ! {
     loop {}
 }
 
+
+
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
